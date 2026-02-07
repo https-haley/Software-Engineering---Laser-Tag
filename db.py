4bd56cf5676a965752d201710776c12c17046b77
Torin Eschberger <torin@eschberger.info>
2026-02-07 13:21:33 -0600

    Created database library

---
+++ db.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+params = {
+    "host": "localhost",
+    "database": "photon",
+    "user": "student",
+    "password": "student"
+}
+
+def get_connection():
+    return psycopg2.connect(**params)
+
+def get_codename(conn, player_id):
+    with conn.cursor() as cur:
+        cur.execute("SELECT codename FROM players WHERE id = %s;", (player_id,))
+        result = cur.fetchone()
+
+        if result:
+            return result[0]
+        return None
+
+def insert_player(conn, player_id, codename):
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO players (id, codename) VALUES (%s, %s)", (player_id, codename))
+    
+    conn.commit()
+
+if __name__ == "__main__":
+    conn = get_connection()
+
+    codename = get_codename(conn, 1)
+    if codename:
+        print(f"Player ID 1 is: {codename}")
+    else:
+        insert_player(conn, 1, "Bob")
+        print(f"Added Bob as player ID 1")
+
+    conn = conn.close()
