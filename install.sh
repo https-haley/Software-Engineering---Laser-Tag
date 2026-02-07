61cc7980de8193173369301987a80b79a79f4d40
Torin Eschberger <torin@eschberger.info>
2026-02-07 12:20:24 -0600

    Updated install script and created requirements.txt for libraries

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-sudo apt install python3-tk
+#!/bin/bash
+
+sudo apt install python3-tk # Install tkinter
+sudo apt-get install python3-venv
+
+# Create virtual environment and install required libraries
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
