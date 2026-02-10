98fc54314b74800d1a6029b8235c0bf768ec9e2f
timothypham045 <timmypham619@gmail.com>
2026-02-10 12:31:42 -0600

    Update player_entry.py
    blank player entry screen

--- player_entry.py
+++ player_entry.py
@@ -1,4 +1,17 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk, messagebox
+import socket
 
 class PlayerEntry: 
     def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Player Entry")
+        self.root.geometry("520x360")
+        self.root.resizable(True, True)
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    app = PlayerEntry()
+    app.run()
