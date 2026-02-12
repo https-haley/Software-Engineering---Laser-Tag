99ceb39b606c109d94a440d03b314f4be7296fc2
timothypham45 <timmypham619@gmail.com>
2026-02-12 01:51:52 -0600

    Working on tables

--- player_entry.py
+++ player_entry.py
@@ -8,6 +8,44 @@ class PlayerEntry:
         self.root.title("Player Entry")
         self.root.geometry("520x360")
         self.root.resizable(True, True)
+        self.root.configure(bg="black")
+            #Main Frame
+        main_frame = tk.Frame(self.root)
+        main_frame.pack()
+
+    #Title
+        title = tk.Label(
+            self.root,
+            text="Edit Current Game",
+            font=("Arial", 18, "bold"),
+            fg="blue",
+            bg = "black"
+        )
+        title.pack(pady=3)
+
+
+        self.create_red_team_table(main_frame)
+        self.create_green_team_table(main_frame)
+
+    def create_red_team_table(self, parent):
+        red_frame = tk.LabelFrame(
+            parent,
+            text="RED TEAM",
+            bg="black",
+            fg="white",
+            font=("Arial", 12 ),
+            padx=10,
+            pady=10
+        )
+        red_frame.grid(row=0, column=0, padx=20)
+
+        self.red_table.pack()
+
+        for i in range(20):
+            self.red_table.insert("", "end", value=(i, "", ""))
+
+    def team_ui(self):
+        self.tree = ttk.Treeview(self, columns)
 
     def run(self):
         self.root.mainloop()
