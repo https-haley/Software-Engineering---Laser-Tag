2b2dc7bdcbe101c45e538a4e19971d3d90f1597a
Torin Eschberger <torin@eschberger.info>
2026-02-14 22:19:49 -0600

    Fixed install script not running on linux

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt install python3-tk # Install tkinter
 sudo apt-get install python3-venv
 
-# Create virtual environment and install required libraries
+# Create virtual environment and install required libs
 python3 -m venv venv
 source venv/bin/activate
 
